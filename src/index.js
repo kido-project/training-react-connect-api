140616182418142e93d72bb24f89f69b89c7ffd7
Kido <kido@Nguyens-MacBook-Pro-2.local>
2021-09-21 23:53:18 +0700

    Integrate react redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import appReducers from './reducers/index';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  appReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={ store }>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
--- src/pages/Products/ProductsPage.js
+++ src/pages/Products/ProductsPage.js
@@ -1,46 +1,11 @@
 import React, { Component } from 'react';
 import Products from './../../components/Products/Products'
 import Product from './../../components/Product/Product'
+import { connect } from 'react-redux';
 
 class ProductsPage extends Component  {
   render() {
-    var products = [
-      {
-        id: 1,
-        code: '101011',
-        name: 'Macbook Pro 2019',
-        price: 1500,
-        status: false,
-      },
-      {
-        id: 2,
-        code: '101012',
-        name: 'Macbook Pro 2020',
-        price: 2000,
-        status: false,
-      },
-      {
-        id: 3,
-        code: '101013',
-        name: 'Macbook Pro 2021',
-        price: 2500,
-        status: true,
-      },
-      {
-        id: 4,
-        code: '101014',
-        name: 'Macbook Pro 2022',
-        price: 3000,
-        status: true,
-      },
-      {
-        id: 5,
-        code: '101015',
-        name: 'Macbook M1',
-        price: 3500,
-        status: true,
-      },
-    ];
+    var { products } = this.props;
     return (
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                       <button type="button" className="btn btn-info mb-10">Add product</button>
@@ -63,5 +28,9 @@ class ProductsPage extends Component  {
     return result;
   }
 }
-
-export default ProductsPage;
+const mapStateToProps = state => {
+  return {
+    products: state.products
+  }
+}
+export default connect(mapStateToProps, null) (ProductsPage);
